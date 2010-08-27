598a128275443d61f8901a3bfaef737960418432
Jelle Helsen <jelle.helsen@hcode.be>
2010-08-27 12:54:48 +0200

    folder selection now works

--- lib/imap_archiver/archiver.rb
+++ lib/imap_archiver/archiver.rb
@@ -1,6 +1,6 @@
 module ImapArchiver
   class Archiver
-    attr_accessor :mailserver, :username, :password, :connection, :auth_mech, :base_folder
+    attr_accessor :mailserver, :username, :password, :connection, :auth_mech, :base_folder, :archive_folder, :folders_to_archive
     def initialize(mailserver,username,password,auth_mech="CRAM-MD5")
       self.mailserver = mailserver
       self.username   = username
@@ -19,30 +19,39 @@ module ImapArchiver
     end
     
     def folder_list
+      if folders_to_archive.is_a? Array
+        folders = folders_to_archive.map {|f| connection.list('',f)}
+        return folders.delete_if {|f| f.nil?}
+      end
+      if folders_to_archive.is_a? Regexp
+        folders = connection.list("","*")
+        return folders.delete_if {|f| (f.name =~ folders_to_archive).nil?}
+      end
       connection.list(base_folder,"*")
     end
     
-    def archive_all    
+    def start 
       folder_list.each do |folder|        
         since_date = Date.today.months_ago(3).beginning_of_month 
         before_date= Date.today.months_ago(3).end_of_month 
-        archive_folder_between_dates(folder.name,since_date,before_date) if folder.name =~ /^Public Folders\/Team\//
+        archive_folder_between_dates(folder.name,since_date,before_date) #if folder.name =~ /^Public Folders\/Team\//
       end
     end
     
     def archive_folder_between_dates(folder, since_date, before_date)
       tmp_folder = Pathname.new(folder).relative_path_from(Pathname.new('Public Folders')).to_s
       archive_folder = "Public Folders/Archief/#{tmp_folder}/#{since_date.strftime("%b %Y")}"
-      puts archive_folder
+      # puts archive_folder
       conditions = ["SINCE", since_date.strftime("%d-%b-%Y"), "BEFORE", before_date.strftime("%d-%b-%Y"), "SEEN", "NOT", "FLAGGED"]
       retry_count = 0
       begin
         connection.select(folder)
+        # puts "will search 1"
         msgs_to_archive = connection.search(conditions)
         if msgs_to_archive.size > 0
+          # puts "will archive #{msgs_to_archive.size} messages"
           if connection.list("",archive_folder).nil?
             connection.create(archive_folder)
-            puts "#{archive_folder} created"
           end
           connection.copy(msgs_to_archive, archive_folder)
           connection.store(msgs_to_archive, "+FLAGS",[:Deleted])
@@ -54,7 +63,7 @@ module ImapArchiver
         end
       rescue IOError => e
         retry_count += 1
-
+        # puts "retrying"
         if retry_count < 3
           reconnect
           retry
@@ -65,6 +74,7 @@ module ImapArchiver
       rescue Exception => e
         retry_count += 1
         if retry_count < 3
+          puts "retrying #{e}"
           retry
         else
           puts "Error archiving #{folder} to #{archive_folder}: #{e}"
--- test/test_imap_archiver.rb
+++ test/test_imap_archiver.rb
@@ -4,6 +4,7 @@ class TestImapArchiver < Test::Unit::TestCase
   context "The archiver" do
     setup do
       @archiver = ImapArchiver::Archiver.new('mailserver','user','password')
+      @archiver.connection = mock()
     end
     should "store it's initialisation parameters" do
       assert_equal('mailserver', @archiver.mailserver)
@@ -24,17 +25,70 @@ class TestImapArchiver < Test::Unit::TestCase
     
     should "call reconnect when calling connect" do
       @archiver.expects(:reconnect)
-      @archiver.reconnect
+      @archiver.connect
     end
     
     should "reconnect when an IOError is raised during archiving" do
-      @archiver.connection = mock()
-      @archiver.connection.stubs(:select)
-      @archiver.connection.stubs(:search).returns([1])
-      @archiver.connection.stubs(:list).returns(true)
-      @archiver.connection.expects(:copy).with([1], 'Public Folders/Archief/test/Mar 2010').at_least_once.raises(IOError)
-      @archiver.expects(:reconnect).at_least_once
+      @archiver.instance_variable_set(:@msg_count, 0)
+      @archiver.connection.expects(:select).times(2)
+      @archiver.connection.expects(:search).times(3).returns([1],[1],[])
+      @archiver.connection.expects(:list).times(2).returns(true)
+      @archiver.connection.stubs(:copy).raises(IOError).then.returns(nil)
+      @archiver.expects(:reconnect).times(1)
+      @archiver.connection.expects(:store)
+      @archiver.connection.expects(:expunge)
       @archiver.archive_folder_between_dates('Public Folders/test',Date.today,Date.today)
     end
+    
+    should "list all folders in base_folder when starting archiving" do
+      @archiver.expects(:folder_list).returns([])
+      @archiver.start
+    end
+    
+    should "copy and delete all found messages" do
+      @archiver.instance_variable_set(:@msg_count, 0)
+      @archiver.connection.expects(:select).at_least_once
+      @archiver.connection.expects(:search).twice.returns([1,5,6],[])
+      @archiver.connection.expects(:list).returns(true)
+      @archiver.connection.expects(:copy).with([1,5,6],"Public Folders/Archief/test/#{Date.today.strftime("%b %Y")}")
+      @archiver.connection.expects(:store)
+      @archiver.connection.expects(:expunge)
+      
+      @archiver.archive_folder_between_dates('Public Folders/test',Date.today,Date.today)
+    end
+    
+    should "list all folders in folders_to_archive array" do
+      @archiver.folders_to_archive = ["Public Folders/test1", "test2"]
+      @archiver.connection.expects(:list).with('',"Public Folders/test1")
+      @archiver.connection.expects(:list).with('',"test2")
+      @archiver.start
+    end
+    
+    should "list all folders in folders_to_archive regexp" do
+      @archiver.folders_to_archive = /testregexp/
+      mock1 = mock(:name => 'hello')
+      mock2 = mock()
+      mock2.expects(:name).twice.returns('testregexp')
+      @archiver.connection.expects(:list).with('',"*").returns([mock1,mock2])
+      @archiver.expects(:archive_folder_between_dates)
+      @archiver.start
+    end
+    
+    should "filter the folder_list when using a regexp" do
+      @archiver.folders_to_archive = /testregexp/
+      mock1 = mock(:name => 'hello')
+      mock2 = mock(:name => 'een testregexp test')
+      @archiver.connection.expects(:list).with('',"*").returns([mock1,mock2])
+      folder_list = @archiver.folder_list
+      assert_equal([mock2], folder_list)
+    end
+    
+    should "strip the folder_list of non-existing mailboxes" do
+      @archiver.folders_to_archive = ["Public Folders/test1", "test2"]
+      @archiver.connection.expects(:list).with('',"Public Folders/test1").returns(mock1 = mock())
+      @archiver.connection.expects(:list).with('',"test2").returns(nil)
+      assert_equal([mock1], @archiver.folder_list)
+    end
+    
   end
 end
