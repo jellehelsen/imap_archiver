18e0a31bb0fac193e35e90b5de21c1cbd0a81c77
Jelle Helsen <jelle.helsen@hcode.be>
2010-09-01 11:09:04 +0200

    upgrading to rspec 2

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,4 +2,16 @@ $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
 require 'imap_archiver'
 
 require "aruba"
-require 'spec/expectations'
+# require 'rspec/expectations'
+# require 'cucumber/rspec/doubles'
+require 'rspec/core'
+require "rspec/mocks"
+RSpec.configure do |c|
+ # c.mock_framework = :rspec
+ c.mock_framework = :mocha
+ # c.mock_framework = :rr
+ # c.mock_framework = :flexmock
+end
+
+require 'cucumber/rspec/doubles'
+# require 'spec/stubs/cucumber'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'imap_archiver'
-require 'spec'
-require 'spec/autorun'
-
-Spec::Runner.configure do |config|
-  
+require 'rspec'
+require 'rspec/autorun'
+require "mocha"
+RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.mock_framework = :mocha
 end
