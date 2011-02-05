adc2e485f5dd7a92b19472e71ddf778d11c029ef
Jelle Helsen <jelle.helsen@hcode.be>
2011-02-05 13:43:14 +0100

    added debug logging

--- lib/imap_archiver/archiver.rb
+++ lib/imap_archiver/archiver.rb
@@ -6,11 +6,16 @@ module ImapArchiver
     attr_accessor :connection
     def initialize(config_file,debug=false)
       @logger = Logger.new(STDOUT)
+      @logger.level = Logger::WARN
       if debug
         @logger.level = Logger::DEBUG
+        @Logger.debug "Debugging enabled."
       end
+      @logger.debug "Loading config"
       self.load_config(config_file)
+      @logger.debug "config loaded"
       config_valid?
+      @logger.debug "config valid"
     end
     
     def connect
@@ -19,18 +24,22 @@ module ImapArchiver
     end
     
     def reconnect
+      @logger.debug "reconnect"
       self.connection = Net::IMAP.new(imap_server) rescue nil
+      @logger.debug "connected"
       capability = self.connection.capability rescue nil
+      @logger.debug "capability: #{capability.inspect}"
       if capability.detect {|c| c =~ /AUTH=(CRAM|DIGEST)-MD5/}
-        # puts "loging in with #{auth_mech}"
+        @logger.debug "loging in with #{auth_mech}"
         self.connection.authenticate(auth_mech,username,password)
       else
-        # puts "plain login"
+        @logger.debug "plain login"
         self.connection.login(username,password) rescue nil
       end
     end
     
     def folder_list
+      @logger.debug "folder_list"
       connect if connection.nil?
       if folders_to_archive.is_a? Array
         folders = folders_to_archive.map {|f| connection.list('',f)}
@@ -44,7 +53,9 @@ module ImapArchiver
     end
     
     def start 
+      @logger.debug "start"
       folder_list.each do |folder|        
+        @logger.debug "starting folder #{folder}"
         since_date = Date.today.months_ago(3).beginning_of_month 
         before_date= Date.today.months_ago(2).beginning_of_month 
         archive_folder_between_dates(folder.name,since_date,before_date) #if folder.name =~ /^Public Folders\/Team\//
@@ -52,17 +63,19 @@ module ImapArchiver
     end
     
     def archive_folder_between_dates(folder, since_date, before_date)
+      @logger.debug "archive_folder_between_dates #{folder}, #{since_date}, #{before_date}"
       tmp_folder = Pathname.new(folder).relative_path_from(Pathname.new(base_folder)).to_s
       current_archive_folder = "#{archive_folder}/#{tmp_folder}/#{since_date.strftime("%b %Y")}"
-      # puts archive_folder
+      @logger.debug "archiving to #{archive_folder}"
       conditions = ["SINCE", since_date.strftime("%d-%b-%Y"), "BEFORE", before_date.strftime("%d-%b-%Y"), "SEEN", "NOT", "FLAGGED"]
+      @logger.debug conditions.join(" ")
       retry_count = 0
       begin
         connection.select(folder)
         # puts "will search 1"
         msgs_to_archive = connection.search(conditions)
         if msgs_to_archive.size > 0
-          # puts "will archive #{msgs_to_archive.size} messages"
+          @logger.debug "will archive #{msgs_to_archive.size} messages"
           if connection.list("",current_archive_folder).nil?
             connection.create(current_archive_folder)
             if connection.capability.include?("ACL")
@@ -81,7 +94,8 @@ module ImapArchiver
         end
       rescue IOError => e
         retry_count += 1
-        # puts "retrying"
+        @logger.debug e.backtrace
+        @logger.debug "retrying"
         if retry_count < 3
           reconnect
           retry
